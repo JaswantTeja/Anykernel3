8a39ed6c6bdb0c285928934c61e3b96836898b16
Nary-Vip <nary2vip@gmail.com>
2021-07-01 11:13:26 +0530

    r5x: NaryTime initiated

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=LordKurama-V2 kernel by Abhishek(LordKurama)
+kernel.string=NaryTime kernel by NareshKumar(NaryVip)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
